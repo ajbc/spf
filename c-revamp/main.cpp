eda8663bef150dee3cd53e925bfc1a7baeb59ed0
ajbc <ajbchaney@gmail.com>
2014-11-26 14:51:00 -0500

    main shell to c-SPF; reads command lline arguments and saves settings; based on Chong's CTR code.

---
+++ c-revamp/main.cpp
@@ -0,0 +1,295 @@
+#include <getopt.h>
+#include "spf.h"
+
+
+#include <stdio.h>
+//#include <string.h>
+//#include "ctr.h"
+
+//gsl_rng * RANDOM_NUMBER = NULL;
+
+void print_usage_and_exit() {
+    // print usage information
+    printf("********************** Social Poisson Factorization (SPF) **********************\n");
+    printf("(c) Copyright 2014 Allison J.B. Chaney  ( achaney@cs.princeton.edu )\n");
+    printf("Distributed under ???; see LICENSE file for details.\n");
+    
+    printf("\nusage:\n");
+    printf(" spf [options]\n");
+    printf("  --help            print help information\n");
+
+    printf("\n");
+    printf("  --out {dir}       save directory, required\n");
+    printf("  --data {dir}      data directory, required\n");
+    
+    printf("\n");
+    printf("  --a_theta {a}     shape hyperparamter to theta (user preferences); default 0.3\n");
+    printf("  --b_theta {b}     rate hyperparamter to theta (user preferences); default 0.3\n");
+    printf("  --a_beta {a}      shape hyperparamter to beta (item attributes); default 0.3\n");
+    printf("  --b_beta {b}      rate hyperparamter to beta (item attributes); default 0.3\n");
+    printf("  --a_tau {a}       shape hyperparamter to tau (user influence); default 2\n");
+    printf("  --b_tau {b}       rate hyperparamter to tau (user influence); default 5\n");
+  
+    printf("\n");
+    printf("  --social-only     only consider social aspect of factorization (SF)\n");
+    printf("  --factor-only     only consider general factors (no social; PF)\n");
+
+    printf("\n");
+    printf("  --binary          assume ratings are binary (instead of default integer)\n");
+    printf("  --directed        assume network is directed (instead of default undirected)\n");
+    
+    printf("\n");
+    printf("  --seed {seed}     the random seed, default from time\n");
+    printf("  --save_lag {lag}  the saving frequency, default 20\n                    -1 means no savings for intermediate results\n");
+    printf("  --max_iter {max}  the max number of iterations, default 200\n");
+    printf("\n");
+
+    printf("  --K {K}           the number of general factors, default 100\n");
+
+    printf("********************************************************************************\n");
+
+    exit(0);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) print_usage_and_exit();
+
+    char filename[500];
+   
+    // variables to store command line args + defaults
+    string out = "";
+    string data = "";
+
+    double a_theta = 0.3;
+    double b_theta = 0.3;
+    double a_beta = 0.3;
+    double b_beta = 0.3;
+    double a_tau = 2;
+    double b_tau = 5;
+
+    // these are really bools, but typed as integers to play nice with getopt
+    int social_only = 0;
+    int factor_only = 0;
+    int binary = 0;
+    int directed = 0;
+
+    time_t t; time(&t);
+    long   seed = (long) t;
+    int    save_lag = 20;
+    int    max_iter = 200;
+
+    int    k = 100;
+
+    // ':' after a character means it takes an argument
+    const char* const short_options = "ho:d:1:2:3:4:5:6:s:l:m:k:";
+    const struct option long_options[] = {
+        {"help",            no_argument,       NULL, 'h'},
+        {"out",             required_argument, NULL, 'o'},
+        {"data",            required_argument, NULL, 'd'},
+        {"a_theta",         required_argument, NULL, '1'},
+        {"b_theta",         required_argument, NULL, '2'},
+        {"a_beta",          required_argument, NULL, '3'},
+        {"b_beta",          required_argument, NULL, '4'},
+        {"a_tau",           required_argument, NULL, '5'},
+        {"b_tau",           required_argument, NULL, '6'},
+        {"social_only",     no_argument, &social_only, 1},
+        {"factor_only",     no_argument, &factor_only, 1},
+        {"binary",          no_argument, &binary, 1},
+        {"directed",        no_argument, &directed, 1},
+        {"seed",            required_argument, NULL, 's'},
+        {"save_lag",        required_argument, NULL, 'l'},
+        {"max_iter",        required_argument, NULL, 'm'},
+        {"K",               required_argument, NULL, 'k'},
+        {NULL, 0, NULL, 0}};
+
+  
+    int opt = 0; 
+    while(true) {
+        opt = getopt_long(argc, argv, short_options, long_options, NULL);
+        switch(opt) {
+            case 'h':
+                print_usage_and_exit();
+                break;
+            case 'o':
+                out = optarg;
+                break;
+            case 'd':
+                data = optarg;
+                break;
+            case '1':
+                a_theta = atof(optarg);
+                break;
+            case '2':
+                b_theta = atof(optarg);
+                break;
+            case '3':
+                a_beta = atof(optarg);
+                break;
+            case '4':
+                b_beta = atof(optarg);
+                break;
+            case '5':
+                a_tau = atof(optarg);
+                break;
+            case '6':
+                b_tau = atof(optarg);
+                break;
+            case 's':
+                seed = atoi(optarg);
+                break;
+            case 'l':
+                save_lag = atoi(optarg);
+                break;
+            case 'm':
+                max_iter =  atoi(optarg);
+                break;    
+            case 'k':
+                k = atoi(optarg);
+                break;
+            case -1:
+                break;
+            case '?':
+                print_usage_and_exit();
+                break;
+            default:
+                break;
+        }
+        if (opt == -1)
+            break;
+    }
+
+    // print information
+    printf("********************************************************************************\n");
+
+    if (out == "") {
+        printf("No output directory specified.  Exiting.\n");
+        exit(-1);
+    }
+    
+    if (!dir_exists(out)) make_directory(out);
+    printf("output directory: %s\n", out.c_str());
+    
+    if (data == "") {
+        printf("No data directory specified.  Exiting.\n");
+        exit(-1);
+    }
+    
+    if (!dir_exists(data)) {
+        printf("data directory %s doesn't exist!  Exiting.\n", data.c_str());
+        exit(-1);
+    }
+    printf("data directory: %s\n", data.c_str());
+
+    if (!file_exists(data + "/train.tsv")) {
+        printf("training data file (train.tsv) doesn't exist!  Exiting.\n");
+        exit(-1);
+    }
+    
+    if (!file_exists(data + "/validation.tsv")) {
+        printf("validation data file (validation.tsv) doesn't exist!  Exiting.\n");
+        exit(-1);
+    }
+    
+    
+    if (social_only && factor_only) {
+        printf("Model cannot be both social only (SF) and factor only (PF).  Exiting.\n");
+        exit(-1);
+    }
+    
+    printf("\nmodel specification:\n");
+    
+    if (social_only) {
+        printf("\tsocial factorization (SF)   [ social factors only ]\n");
+    } else if (factor_only) {
+        printf("\tPoisson factorization (PF)   [ general preference factors only ]\n");
+    } else {
+        printf("\tsocial Poisson factorization (SPF)\n");
+    }
+    if (!social_only) {
+        printf("\tK = %d   (number of latent factors for general preferences)\n", k);
+    }
+
+    printf("\nshape and rate hyperparameters:\n");
+    if (!social_only) {
+        printf("\ttheta (%.2f, %.2f)\n", a_theta, b_theta);
+        printf("\tbeta  (%.2f, %.2f)\n", a_beta, b_beta);
+    }
+    if (!factor_only) {
+        printf("\ttau   (%.2f, %.2f)\n", a_tau, b_tau);
+    }
+    
+
+    printf("\ndata attributes:\n");
+    
+    if (binary) {
+        printf("\tbinary ratings\n");
+    } else {
+        printf("\tinteger ratings\n");
+    }
+    
+    if (!factor_only) {
+        if (directed) {
+            printf("\tdirected network\n");
+        } else {
+            printf("\tundirected network\n");
+        }
+    }
+
+    
+    printf("\ninference parameters:\n");
+    printf("\tseed:                         %d\n", (int)seed);
+    printf("\tsave lag:                     %d\n", save_lag);
+    printf("\tmaximum number of iterations: %d\n", max_iter);
+    
+
+    // save the settings
+    printf("\nSAVING SETTINGS\n");
+    model_settings settings;
+    settings.set(out, data, a_theta, b_theta, a_beta, b_beta, a_tau, b_tau,
+        (bool) social_only, (bool) factor_only, (bool) binary, (bool) directed,
+        seed, max_iter, save_lag, k);
+    settings.save(out + "/settings.txt");
+ 
+ /* 
+  /// init random numbe generator
+  RANDOM_NUMBER = new_random_number_generator(random_seed);
+
+  // read users
+  printf("reading user matrix from %s ...\n", user_path);
+  c_data* users = new c_data(); 
+  users->read_data(user_path);
+  int num_users = (int)users->m_vec_data.size();
+
+  // read items
+  printf("reading item matrix from %s ...\n", item_path);
+  c_data* items = new c_data(); 
+  items->read_data(item_path);
+  int num_items = (int)items->m_vec_data.size();
+
+  // create model instance
+  printf("model instance gutted for now\n");
+  /*c_ctr* ctr = new c_ctr();
+  ctr->set_model_parameters(num_factors, num_users, num_items);
+
+  c_corpus* c = NULL;
+  if (mult_path != NULL) {
+    // read word data
+    c = new c_corpus();
+    c->read_data(mult_path);
+    ctr->read_init_information(theta_init_path, beta_init_path, c, alpha_smooth);
+  }
+
+  if (learning_rate <= 0) {
+    ctr->learn_map_estimate(users, items, c, &ctr_param, directory);
+  } else {
+    ctr->stochastic_learn_map_estimate(users, items, c, &ctr_param, directory);
+  }*
+
+  free_random_number_generator(RANDOM_NUMBER);
+  if (c != NULL) delete c;
+
+  //delete ctr;
+  delete users;
+  delete items;*/
+  return 0;
+}
---
+++ c-revamp/spf.h
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <armadillo>
+
+#include "utils.h"
+
+using namespace std;
+using namespace arma;
+
+struct model_settings {
+    string outdir;
+    string datadir;
+
+    double a_theta;
+    double b_theta;
+    double a_beta;
+    double b_beta;
+    double a_tau;
+    double b_tau;
+
+    bool social_only;
+    bool factor_only;
+    bool binary;
+    bool directed;
+    
+    long seed;
+    int  save_lag;
+    int  max_iter;
+
+    int k;
+  
+    
+    void set(string out, string data, 
+             double athe, double bthe, double abet, double bbet, 
+             double atau, double btau,
+             bool social, bool factor, bool bin, bool dir,
+             long rand, int lag, int iter,
+             int num_factors) {
+        outdir = out;
+        datadir = data;
+        
+        a_theta = athe;
+        b_theta = bthe;
+        a_beta  = abet;
+        b_beta  = bbet;
+        a_tau   = atau;
+        b_tau   = btau;
+
+        social_only = social;
+        factor_only = factor;
+        binary = bin;
+        directed = dir;
+
+        seed = rand;
+        save_lag = lag;
+        max_iter = iter;
+
+        k = num_factors;
+    }
+
+    void save(string filename) {
+        FILE * file = fopen(filename.c_str(), "w");
+        
+        fprintf(file, "data directory: %s\n", datadir.c_str());
+
+        fprintf(file, "\nmodel specification:\n");
+        if (social_only) {
+            fprintf(file, "\tsocial factorization (SF)   [ social factors only ]\n");
+        } else if (factor_only) {
+            fprintf(file, "\tPoisson factorization (PF)   [ general preference factors only ]\n");
+        } else {
+            fprintf(file, "\tsocial Poisson factorization (SPF)\n");
+        }
+        if (!social_only) {
+            fprintf(file, "\tK = %d   (number of latent factors for general preferences)\n", k);
+        }
+
+    
+        fprintf(file, "\nshape and rate hyperparameters:\n");
+        if (!social_only) {
+            fprintf(file, "\ttheta (%f, %f)\n", a_theta, b_theta);
+            fprintf(file, "\tbeta  (%f, %f)\n", a_beta, b_beta);
+        }
+        if (!factor_only) {
+            fprintf(file, "\ttau   (%f, %f)\n", a_tau, b_tau);
+        }
+        
+
+        fprintf(file, "\ndata attributes:\n");
+        
+        if (binary) {
+            fprintf(file, "\tbinary ratings\n");
+        } else {
+            fprintf(file, "\tinteger ratings\n");
+        }
+        
+        if (!factor_only) {
+            if (directed) {
+                fprintf(file, "\tdirected network\n");
+            } else {
+                fprintf(file, "\tundirected network\n");
+            }
+        }
+
+        
+        fprintf(file, "\ninference parameters:\n");
+        fprintf(file, "\tseed:                         %d\n", (int)seed);
+        fprintf(file, "\tsave lag:                     %d\n", save_lag);
+        fprintf(file, "\tmaximum number of iterations: %d\n", max_iter);
+    
+        fclose(file);
+    }
+};
