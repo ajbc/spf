9e969cd7b26fa9e1a4d54caccc00a244cc087738
ajbc <ajbchaney@gmail.com>
2014-11-26 19:21:31 -0500

    added user and item id maps

--- c-revamp/data.cpp
+++ c-revamp/data.cpp
@@ -6,6 +6,20 @@ Data::Data(bool bin, bool dir) {
 }
 
 void Data::read_ratings(string filename) {
+    FILE *fileptr = fopen(filename.c_str(), "r");
+
+    int user, item, rating;
+    while ((fscanf(fileptr, "%d\t%d\t%d\n", &user, &item, &rating) != EOF)) {
+        printf("user %d, item %d, rating %d\n", user, item, rating);
+
+        // map user and item ids
+        if (user_ids.count(user) == 0)
+            user_ids[user] = user_ids.size(); 
+        if (item_ids.count(item) == 0)    
+            item_ids[item] = item_ids.size();
+
+    }
+    fclose(fileptr);
 }
 
 void Data::read_network(string filename) {
@@ -15,4 +29,12 @@ void Data::read_validation(string filename) {
 }
 
 void Data::save_summary(string filename) {
+    FILE * file = fopen(filename.c_str(), "w");
+    printf("TODO\n");
+    
+    fprintf(file, "num users:\t%d\n", user_ids.size());
+    fprintf(file, "num items:\t%d\n", item_ids.size());
+    fprintf(file, "num ratings:\t%d\n", 0);
+    fprintf(file, "network connections:\t%d\n", 0);
+    fclose(file);
 }
--- c-revamp/data.h
+++ c-revamp/data.h
@@ -2,12 +2,16 @@
 #define DATA_H
 
 #include <string>
+#include <stdio.h>
+#include <map>
 using namespace std;
 
 class Data {
     private:
         bool binary;
         bool directed;
+        map<int,int> user_ids;
+        map<int,int> item_ids;
 
     public:
         Data(bool bin, bool dir);
@@ -17,69 +21,4 @@ class Data {
         void save_summary(string filename);
 };
 
-
-// dataset class
-//
-// TODO:
-//
-
-    /*fout = open(join(args.out_dir, "data_stats.dat"), 'w+')
-    fout.write("num users:\t%d\n" % len(data.users))
-    fout.write("num items:\t%d\n" % len(data.items))
-    fout.write("num ratings:\t%d\t%d\t%d\n" % \
-        (data.rating_count[0], data.rating_count[1], data.rating_count[2]))
-    fout.write("network connections:\t%d\n" % data.connection_count)
-    fout.close()*/
-    /*void save(string filename) {
-        FILE * file = fopen(filename.c_str(), "w");
-        
-        fprintf(file, "data directory: %s\n", datadir.c_str());
-
-        fprintf(file, "\nmodel specification:\n");
-        if (social_only) {
-            fprintf(file, "\tsocial factorization (SF)   [ social factors only ]\n");
-        } else if (factor_only) {
-            fprintf(file, "\tPoisson factorization (PF)   [ general preference factors only ]\n");
-        } else {
-            fprintf(file, "\tsocial Poisson factorization (SPF)\n");
-        }
-        if (!social_only) {
-            fprintf(file, "\tK = %d   (number of latent factors for general preferences)\n", k);
-        }
-
-    
-        fprintf(file, "\nshape and rate hyperparameters:\n");
-        if (!social_only) {
-            fprintf(file, "\ttheta (%f, %f)\n", a_theta, b_theta);
-            fprintf(file, "\tbeta  (%f, %f)\n", a_beta, b_beta);
-        }
-        if (!factor_only) {
-            fprintf(file, "\ttau   (%f, %f)\n", a_tau, b_tau);
-        }
-        
-
-        fprintf(file, "\ndata attributes:\n");
-        
-        if (binary) {
-            fprintf(file, "\tbinary ratings\n");
-        } else {
-            fprintf(file, "\tinteger ratings\n");
-        }
-        
-        if (!factor_only) {
-            if (directed) {
-                fprintf(file, "\tdirected network\n");
-            } else {
-                fprintf(file, "\tundirected network\n");
-            }
-        }
-
-        
-        fprintf(file, "\ninference parameters:\n");
-        fprintf(file, "\tseed:                         %d\n", (int)seed);
-        fprintf(file, "\tsave lag:                     %d\n", save_lag);
-        fprintf(file, "\tmaximum number of iterations: %d\n", max_iter);
-    
-        fclose(file);
-    }*/
 #endif
