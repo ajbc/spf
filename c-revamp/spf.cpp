4a7d0a2a11b31c443582266c104de960ef423ef0
ajbc <ajbchaney@gmail.com>
2014-12-16 12:14:58 -0500

    added ndcg

--- c-revamp/spf.cpp
+++ c-revamp/spf.cpp
@@ -149,7 +149,7 @@ void SPF::evaluate_rankings() {
     // compute eval metrics from ratings/rankings
     FILE* file = fopen((settings->outdir+"/rankings.tsv").c_str(), "r");
     FILE* user_file = fopen((settings->outdir+"/user_eval.tsv").c_str(), "w");
-    fprintf(user_file, "user.id\tnum.heldout\trmse\tmae\tave.rank\tfirst\tcrr\tncrr\n");
+    fprintf(user_file, "user.id\tnum.heldout\trmse\tmae\tave.rank\tfirst\tcrr\tncrr\tndcg\n");
 
     // overall attributes to track
     int user_count = 0;
@@ -166,6 +166,7 @@ void SPF::evaluate_rankings() {
     double user_sum_first = 0;
     double user_sum_crr = 0;
     double user_sum_ncrr = 0;
+    double user_sum_ndcg = 0;
 
     // per user attibutes
     double user_rmse = 0;
@@ -176,6 +177,8 @@ void SPF::evaluate_rankings() {
     double user_crr = 0;
     double user_ncrr = 0;
     double user_ncrr_normalizer = 0;
+    double user_ndcg = 0;
+    double user_ndcg_normalizer = 0;
         
     // per line variables
     int user, item, rating, rank;
@@ -192,15 +195,17 @@ void SPF::evaluate_rankings() {
                 user_mae /= user_heldout;
                 user_rank /= user_heldout;
                 user_ncrr = user_crr / user_ncrr_normalizer;
+                user_ndcg /= user_ndcg_normalizer;
                 
                 log_user(user_file, prev_user, user_heldout, user_rmse, 
-                    user_mae, user_rank, first, user_crr, user_ncrr);
+                    user_mae, user_rank, first, user_crr, user_ncrr, user_ndcg);
                 user_sum_rmse += user_rmse;
                 user_sum_mae += user_mae;
                 user_sum_rank += user_rank;
                 user_sum_first += first;
                 user_sum_crr += user_crr;
                 user_sum_ncrr += user_ncrr;
+                user_sum_ndcg += user_ndcg;
             }
 
             user_count++;
@@ -210,6 +215,8 @@ void SPF::evaluate_rankings() {
             first = 0;
             user_crr = 0;
             user_ncrr_normalizer = 0;
+            user_ndcg = 0;
+            user_ndcg_normalizer = 0;
 
             user_heldout = 0;
             prev_user = user;
@@ -236,6 +243,9 @@ void SPF::evaluate_rankings() {
             crr += local_metric;
             user_ncrr_normalizer += 1.0 / user_heldout;
 
+            user_ndcg += rating / log(rank + 1);
+            user_ndcg_normalizer += rating / log(user_heldout + 1);
+
             if (first == 0)
                 first = rank;
         }
@@ -245,8 +255,9 @@ void SPF::evaluate_rankings() {
     user_mae /= user_heldout;
     user_rank /= user_heldout;
     user_ncrr = user_crr / user_ncrr_normalizer;
+    user_ndcg /= user_ndcg_normalizer;
     log_user(user_file, user, user_heldout, user_rmse, user_mae, user_rank,
-        first, user_crr, user_ncrr);
+        first, user_crr, user_ncrr, user_ndcg);
     // aggregate metrics
     user_sum_rmse += user_rmse;
     user_sum_mae += user_mae;
@@ -254,6 +265,7 @@ void SPF::evaluate_rankings() {
     user_sum_first += first;
     user_sum_crr += user_crr;
     user_sum_ncrr += user_ncrr;
+    user_sum_ndcg += user_ndcg;
     user_count++;
 
     fclose(file);
@@ -271,7 +283,7 @@ void SPF::evaluate_rankings() {
     fprintf(file, "first\t%f\t---\n", user_sum_first/user_count);
     fprintf(file, "CRR\t%f\t%f\n", user_sum_crr/user_count, crr/heldout_count);
     fprintf(file, "NCRR\t%f\t---\n", user_sum_ncrr/user_count);
-    //fprintf(file, "NDCG\t%f\t---\n", user_sum_ndcg/user_count);
+    fprintf(file, "NDCG\t%f\t---\n", user_sum_ndcg/user_count);
     //fprintf(file, "prec@1\t%f\t%f\n", user_sum_p1/user_count, 
     //    precision1/heldout_count);
     //fprintf(file, "prec@10\t%f\t%f\n", user_sum_p10/user_count, 
@@ -453,7 +465,7 @@ void SPF::log_convergence(int iteration, double ave_ll, double delta_ll) {
 }
 
 void SPF::log_user(FILE* file, int user, int heldout, double rmse, double mae,
-    double rank, int first, double crr, double ncrr) {
-    fprintf(file, "%d\t%d\t%f\t%f\t%f\t%d\t%f\t%f\n", 
-        user, heldout, rmse, mae, rank, first, crr, ncrr);
+    double rank, int first, double crr, double ncrr, double ndcg) {
+    fprintf(file, "%d\t%d\t%f\t%f\t%f\t%d\t%f\t%f\t%f\n", 
+        user, heldout, rmse, mae, rank, first, crr, ncrr, ndcg);
 }
--- c-revamp/spf.h
+++ c-revamp/spf.h
@@ -153,7 +153,8 @@ class SPF {
         double get_ave_log_likelihood();
         void log_convergence(int iteration, double ave_ll, double delta_ll);
         void log_user(FILE* file, int user, int heldout, double rmse, 
-            double mae, double rank, int first, double crr, double ncrr);
+            double mae, double rank, int first, double crr, double ncrr,
+            double ndcg);
 
         
     public:
