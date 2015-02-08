c7f1223a47a1455b638d9fb3af5d10629153863f
ajbc <ajbchaney@gmail.com>
2015-02-08 09:07:37 -0500

    added study.sh script

---
+++ c-revamp/study.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "usage: ./study [data-dir] [output-dir] [K]"
+    exit
+fi
+
+
+datadir=$1
+outdir=$2
+K=$3
+
+if [ ! -d $2 ]; then
+    mkdir $2
+fi
+
+seed=948237247
+
+echo " * initializing study of main model (this will launch multiple processes"
+echo "   that will continue living after this bash script has completed)"
+
+mkdir $2/spf
+mkdir $2/pf
+mkdir $2/sf
+
+(./spf --data $1 --out $2/spf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --min_iter 40 --max_iter 9999 --final_pass > $2/spf/out 2> $2/spf/err &)
+(./spf --data $1 --out $2/pf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --factor_only --min_iter 40 --max_iter 9999 --final_pass > $2/pf/out 2> $2/pf/err &)
+(./spf --data $1 --out $2/sf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --social_only --min_iter 40 --max_iter 9999 --final_pass > $2/sf/out 2> $2/sf/err &)
