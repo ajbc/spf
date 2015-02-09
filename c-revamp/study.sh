19d2b7104eea98be9865e34044613bfd5115e758
ajbc <ajbchaney@gmail.com>
2015-02-09 12:02:41 -0500

    change min number of iterations for study

--- c-revamp/study.sh
+++ c-revamp/study.sh
@@ -31,7 +31,7 @@ make pop
 echo " * initializing study of main model (this will launch multiple processes"
 echo "   that will continue living after this bash script has completed)"
 
-(./spf --data $1 --out $2/spf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --min_iter 40 --max_iter 9999 --final_pass > $2/spf/out 2> $2/spf/err &)
-(./spf --data $1 --out $2/pf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --factor_only --min_iter 40 --max_iter 9999 --final_pass > $2/pf/out 2> $2/pf/err &)
-(./spf --data $1 --out $2/sf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --social_only --min_iter 40 --max_iter 9999 --final_pass > $2/sf/out 2> $2/sf/err &)
+(./spf --data $1 --out $2/spf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --min_iter 100 --max_iter 9999 --final_pass > $2/spf/out 2> $2/spf/err &)
+(./spf --data $1 --out $2/pf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --factor_only --min_iter 100 --max_iter 9999 --final_pass > $2/pf/out 2> $2/pf/err &)
+(./spf --data $1 --out $2/sf --binary --directed --svi --K $K --seed $seed --save_freq 1000 --conv_freq 100 --social_only --min_iter 100 --max_iter 9999 --final_pass > $2/sf/out 2> $2/sf/err &)
 (./pop --data $1 --out $2/pop > $2/pop/out 2> $2/pop/err &)
